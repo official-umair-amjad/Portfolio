52f3f4a38f5e7305d6591f69f8c0ddb27798867b
Muhammad Umair <112547986+official-umair-amjad@users.noreply.github.com>
2025-02-14 19:30:31 +0500

    Add files via upload

--- components/organisms/AppSignature.jsx
+++ components/organisms/AppSignature.jsx
@@ -5,12 +5,23 @@ const AppSignature = () => (
   <AppSection decoration={false}>
     <div className="text-center">
       <p className="text-primary text-xs md:text-sm">
-          Greetings From
+        Greetings From
       </p>
       <h3 className="font-signature text-xl md:text-3xl inline-block py-1 mt-3 md:mt-6 border-b text-primary">
         Muhammad Umair Amjad
-      </h3><br/>
-      <AppButton title="VIEW MY RESUME" href="/Resume.pdf" target="_blank" />
+      </h3><br />
+      <div className='inline-block w-max p-2 mt-6 border border-light-gray'>
+
+        <a
+          href={'/Resume.pdf'}
+          target="_blank"
+          rel="noopener noreferrer"
+          className="inline-block min-w-[200px] text-xs lg:text-sm text-center font-bold bg-primary py-3 px-9 hover:bg-black text-white hover:text-white transition duration-300"
+        >
+          VIEW MY RESUME
+        </a>
+      </div>
+      
     </div>
   </AppSection>
 );
